66cc0c5deddf906494a90816fe4da7957cd3e8ac
Dragan <milosevicdragan002@gmail.com>
2023-10-18 14:13:43 +0200

    Creating a New Table

---
+++ Vezbe1/practice.sql
@@ -0,0 +1,12 @@
+--Kreiranje nove tabele
+CREATE TABLE faze_projekta(
+    Spr integer not null, --šifra faze projekta,
+    Sfp integer not null, --sifra projekta,
+    Rukfp integer, --rukovodilac faze projekta,
+    Nafp varchar2(20), --naziv faze projekta,
+    Datp date, --datum početka faze projekta
+    CONSTRAINT faze_projekta_PK PRIMARY KEY (spr,sfp),
+    CONSTRAINT faze_projekta_fk1 FOREIGN KEY (spr) REFERENCES projekat (spr),
+    CONSTRAINT faze_projekta_fk2 FOREIGN KEY (rukfp) REFERENCES radnik (mbr),
+    CONSTRAINT faze_projekta_uk UNIQUE(nafp)
+);
