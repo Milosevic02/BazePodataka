10de951bbcb3611fac07228845b83074547d3181
Dragan <milosevicdragan002@gmail.com>
2023-10-26 13:09:13 +0200

    AVG

--- Vezbe2/practice.sql
+++ Vezbe2/practice.sql
@@ -30,3 +30,7 @@ SELECT MIN(plt) minimalna_plt, MAX(plt) maksimalna_plt
 --SUM
 SELECT COUNT(mbr) "Broj radnika",SUM(plt) "Ukupna mesecna plata"
     FROM radnik;
+
+--AVG 
+SELECT COUNT(*) "Broj radnika",AVG(plt) "Prosecna plata",12*SUM(plt) "Godisnja plata"
+    FROM radnik;
