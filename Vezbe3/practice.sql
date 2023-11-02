5c0308f707d157872ffde025f6483572a4fadf24
Dragan <milosevicdragan002@gmail.com>
2023-11-02 13:18:03 +0100

    RIGHT OUTER JOIN

--- Vezbe3/practice.sql
+++ Vezbe3/practice.sql
@@ -96,10 +96,14 @@ SELECT *
     FROM radnik r INNER JOIN radproj rp ON r.mbr = rp.mbr
     WHERE spr = 30;
 
---LEFT OUTER JOIN -> Lepi desnu na levu tabelu
+--LEFT OUTER JOIN -> Zadrzava levu tabelu i prikazuje null u desnoj tabeli
 SELECT *
     FROM radnik r LEFT OUTER JOIN radproj rp ON r.mbr = rp.mbr;
 
 --LEFT OUTER JOIN zad1 -> Ispisati ime prz mbr i naziv projekta kojim rukovodi ako ne rukovodi ni jednim ispisati ne rukovodi
 SELECT r.mbr,r.ime,r.prz,NVL(nap,'Ne rukovodi projektom') Projekat
     FROM radnik r LEFT OUTER JOIN projekat p ON r.mbr = p.ruk;
+
+--RIGHT OUTER JOIN -> Zadrzava desnu tabelu i prikazuje null u levoj tabeli
+SELECT *
+    FROM radproj rp RIGHT OUTER JOIN projekat p ON rp.spr = p.spr;
