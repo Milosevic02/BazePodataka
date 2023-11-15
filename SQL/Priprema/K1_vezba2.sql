92200d68ec2b5b2b107f5e6b9b733b9c30669862
Dragan <milosevicdragan002@gmail.com>
2023-11-15 13:04:45 +0100

    Zadatak 3

--- SQL/Priprema/K1_vezba2.sql
+++ SQL/Priprema/K1_vezba2.sql
@@ -1,9 +1,14 @@
---1.Prikazati imena i prezimena svih radnika sortirano u opadajucem redosledu na osnovu imena
+--1. Prikazati imena i prezimena svih radnika sortirano u opadajucem redosledu na osnovu imena
 SELECT ime,prz
     FROM radnik 
     ORDER BY ime DESC;
 
---Povecati plate za 30% svim radnicima ciji je maticni broj 70 i manji
+--2. Povecati plate za 30% svim radnicima ciji je maticni broj 70 i manji
 UPDATE radnik
     SET plt = plt*1.30
     WHERE mbr <= 70;
+
+--3. Ispisati maticni broj, ime i prezime radnika cije prezime pocinje na slovo M dok ime ne pocinje slovom M.
+SELECT mbr,ime,prz
+    FROM radnik
+    WHERE prz LIKE 'M%' and ime NOT LIKE 'M%';
