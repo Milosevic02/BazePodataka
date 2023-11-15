ee53ac50ce48b3f81e66b4b4e2cfa801d24373ce
Dragan <milosevicdragan002@gmail.com>
2023-11-15 13:03:18 +0100

    Zadatak2

--- SQL/Priprema/K1_vezba2.sql
+++ SQL/Priprema/K1_vezba2.sql
@@ -2,4 +2,8 @@
 SELECT ime,prz
     FROM radnik 
     ORDER BY ime DESC;
-    
+
+--Povecati plate za 30% svim radnicima ciji je maticni broj 70 i manji
+UPDATE radnik
+    SET plt = plt*1.30
+    WHERE mbr <= 70;
