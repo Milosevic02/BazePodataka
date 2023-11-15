7af848edacbe7bed32c527105cb222fe0c9d7b5d
Dragan <milosevicdragan002@gmail.com>
2023-11-15 08:48:39 +0100

    Zadatak2

--- SQL/Priprema/K1_vezba.sql
+++ SQL/Priprema/K1_vezba.sql
@@ -3,3 +3,10 @@
 SELECT product_name,standard_cost,list_price
     FROM products
     ORDER BY list_price,standard_cost DESC;
+
+--2. Prikazati proizvode (products.product_name, products.description) i razlike
+--između prodajne i nabavne cene za proizvode čiji nazivi počinju sa 'Intel'.
+SELECT product_name,description,list_price - standard_cost
+    FROM products
+    WHERE product_name LIKE 'Intel%';
+
