dac31d17885dacc74dd96cd163dde2b8ada7276b
Dragan <milosevicdragan002@gmail.com>
2023-11-16 08:29:55 +0100

    Zadatak2

--- SQL/Priprema/K1_vezba3.sql
+++ SQL/Priprema/K1_vezba3.sql
@@ -2,3 +2,9 @@
 SELECT idf,nazivf 
     FROM film 
     ORDER BY nazivf;
+
+--2. Prikazati sve filmove (IDF, NAZIVF, TRAJANJEF) čije trajanje je u
+-- opsegu [100, 150] minuta
+SELECT idf,nazivf,trajanjef
+    FROM film
+    WHERE trajanjef BETWEEN 100 and 150;
