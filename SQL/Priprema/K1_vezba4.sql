e2f7367deaa6e069901bd23c6a18b910e1926ed2
Dragan <milosevicdragan002@gmail.com>
2023-11-16 14:21:13 +0100

    Zadatak 4

--- SQL/Priprema/K1_vezba4.sql
+++ SQL/Priprema/K1_vezba4.sql
@@ -16,4 +16,10 @@ ADD(geo_sirina DECIMAL(10,8) DEFAULT NULL);
 ALTER TABLE staza
 ADD(geo_duzina DECIMAL(11,8) DEFAULT NULL);
 
+--4.Prikazati staze (idenfikacionu oznaku staze, naziv staze i naziv države u kojoj se staza nalazi) na kojima je barem jedan vozač 2019. godine ostvario
+-- broj poena manji od prosečnog broja poena za sve vožnje iz 2019. godine. Ukoliko su na nekoj stazi dva ili više vozača ostvarila takav broj bodova, stazu prikazati samo jednom.
+SELECT DISTINCT ids,nazivs,drzs
+    FROM staza INNER JOIN rezultat ON ids = stazar
+        INNER JOIN vozac ON idv = vozacr
+    WHERE sezona = 2019 AND bodovi < (SELECT AVG(bodovi) FROM rezultat WHERE sezona = 2019);
 
