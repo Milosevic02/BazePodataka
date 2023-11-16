c459dc296e90bc1fa7824efe2edf2472b3af2d44
Dragan <milosevicdragan002@gmail.com>
2023-11-16 14:06:56 +0100

    Zadatak 2

--- SQL/Priprema/K1_vezba4.sql
+++ SQL/Priprema/K1_vezba4.sql
@@ -2,3 +2,8 @@
 SELECT idd,nazivd 
     FROM drzava
     ORDER BY nazivd;
+
+--2. Prikazati sve vozače (IDV, IMEV, PREZV) koji u imenu malo slovo ‘n’ ili veliko slovo ‘L’
+SELECT idv,imev,prezv
+    FROM vozac
+    WHERE imev LIKE '%n%' OR imev LIKE '%L%';
