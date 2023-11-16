567a29c20150536dd3f80130b94aa2c015e86aa5
Dragan <milosevicdragan002@gmail.com>
2023-11-16 08:47:23 +0100

    Zadatak4

--- SQL/Priprema/K1_vezba3.sql
+++ SQL/Priprema/K1_vezba3.sql
@@ -13,4 +13,10 @@ SELECT idf,nazivf,trajanjef
 -- mora biti u opsegu [1, 10].
 ALTER TABLE Ocena
     ADD CONSTRAINT check_ocena_range CHECK (OCENAO >= 1 AND OCENAO <= 10);
-    
+
+--4. Prikazati korisnike (KORIMEK, IMEK, PRZK) koji su ocenili barem jedan od filmova koji su snimljeni nakon prosečne godine snimanja svih
+--filmova. Ukoliko je jedan korisnik ocenio više ovakvih filmova, prikazati ga samo jednom.
+SELECT DISTINCT korimek,imek,przk 
+    FROM film f INNER JOIN ocena o ON idf = filmo
+            INNER JOIN korisnik k ON korimeo = korimek
+    WHERE f.godf > (SELECT AVG(f2.godf) FROM film f2)
